4746aee708f13acba503a764561a27942d1a3de3
SiddhantDembi <siddhantdembi@gmail.com>
2024-07-01 15:27:24 +0530

    login

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_DATABASE = os.getenv("DB_DATABASE")
-LOGIN_PASSWORD = os.getenv("DB_DATABASE")
+LOGIN_PASSWORD = os.getenv("LOGIN_PASSWORD")
 
 # Create a MySQL connection
 db_connection = mysql.connector.connect(
